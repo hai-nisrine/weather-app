5a6603f8beae9b68b84d95ca7ef1c3ca0ed5f9c7
Nisrine Haida <nisrine.haida.77@gmail.com>
2026-07-21 15:40:13 +0100

    fix js file

--- index.js
+++ index.js
@@ -1,27 +1,51 @@
+const inputEl = document.querySelector("#input-el")
+const cityEl = document.querySelector("#city-el")
+const temperatureEl = document.querySelector("#temperature-el")
+const searchBtn = document.querySelector("#search-btn")
 
 
-// fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true")
-// .then(response => {
-//     if (!response.ok) {
-
-//     }
-//     })
-// .then(data => console.log(data.current_weather))
-// .catch(error => console.error(error))
-
-fetchData()
-
-async function fetchData() {
+async function fetchData(city) {
     try {
-        const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true")
 
-        if (!response.ok) {
-            throw new Error("Could not fetch resource")
+        //get city coordinates
+
+        const geoResponce = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}`)
+
+        if (!geoResponce.ok) {
+            throw new Error("Could not fetch location")
+        }
+        
+        const geoData = await geoResponce.json()
+        
+        if (!geoData.results) {
+            throw new Error("Could not find city")
         }
 
-        const data = await response.json()
-        console.log(data.current_weather)
+        const { latitude, longitude, name} = geoData.results[0]
+
+
+        //get city weather 
+        const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
+
+        if (!weatherResponse.ok) {
+            throw new Error("Could not fetch weather")
+        }
+
+        const weatherData = await weatherResponse.json()
+
+
+        cityEl.textContent = name;
+        temperatureEl.textContent = weatherData.current_weather.temperature
+       
     } catch(error) {
-        console.log("Error")
+        console.log(error)
     }
 }
+
+
+
+searchBtn.addEventListener("click", function() {
+    const city = inputEl.value
+    fetchData(city)
+
+})
